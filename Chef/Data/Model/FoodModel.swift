e6d81705569d94ec8f1676b1a0528c834d30bd8e
takuto277 <takuto.72.soccer@gmail.com>
2024-11-21 02:02:46 +0900

    add FoodModel

--- Chef/Data/Model/FoodModel.swift
+++ Chef/Data/Model/FoodModel.swift
@@ -5,3 +5,15 @@
 //  Created by 小野拓人 on 2024/11/21.
 //
 
+import SwiftData
+
+@Model
+class Food: PersistentModel {
+    var name: String
+    var calories: Int
+
+    init(name: String, calories: Int) {
+        self.name = name
+        self.calories = calories
+    }
+}
