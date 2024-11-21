bb305981bd80e7ce3508bf906dd03f806b83cc9d
takuto277 <takuto.72.soccer@gmail.com>
2024-11-22 02:06:02 +0900

    fix firstView

--- Chef/ChefApp.swift
+++ Chef/ChefApp.swift
@@ -25,7 +25,7 @@ struct ChefApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SplashView()
         }
         .modelContainer(sharedModelContainer)
     }
