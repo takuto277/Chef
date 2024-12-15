1576a8ea2022f613522b3cd5d60d4cbdd049487f
takuto277 <takuto.72.soccer@gmail.com>
2024-12-16 00:10:17 +0900

    add foodDetailView

---
+++ Chef/Presenter/UI/FoodDetail/FoodDetailView.swift
@@ -0,0 +1,14 @@
+//
+//  FoodDetailView.swift
+//  Chef
+//
+//  Created by 小野拓人 on 2024/12/15.
+//
+
+import SwiftUI
+
+struct FoodDetailView: View {
+    var body: some View {
+        Text("フード詳細画面")
+    }
+}
