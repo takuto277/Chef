2a2b4695d3de44feef0c7d38af053656deb96bcf
takuto277 <takuto.72.soccer@gmail.com>
2025-03-16 22:48:42 +0900

    add SegmentFood

---
+++ Chef/Data/Model/SegmentFood.swift
@@ -0,0 +1,23 @@
+//
+//  SegmentFood.swift
+//  Chef
+//
+//  Created by 小野拓人 on 2025/03/16.
+//
+
+import Foundation
+import UIKit
+
+struct SegmentedFood: Identifiable {
+    let id: UUID
+    let name: String
+    let quantity: String
+    let image: UIImage
+    
+    init(id: UUID, name: String, quantity: String, image: UIImage) {
+        self.id = id
+        self.name = name
+        self.quantity = quantity
+        self.image = image
+    }
+}
