241b5c664919a884206c4fd29d7587b84fef1b0b
takuto277 <takuto.72.soccer@gmail.com>
2025-03-20 17:10:01 +0900

    add editableImagePicker

---
+++ Chef/Presenter/Utils/EditableImagePicker.swift
@@ -0,0 +1,46 @@
+//
+//  EditableImagePicker.swift
+//  Chef
+//
+//  Created by 小野拓人 on 2025/03/20.
+//
+
+import SwiftUI
+import UIKit
+
+
+struct EditableImagePicker: UIViewControllerRepresentable {
+    @Binding var selectedImage: UIImage?
+    @Environment(\.presentationMode) var presentationMode
+    
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        picker.allowsEditing = true
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        let parent: EditableImagePicker
+        
+        init(_ parent: EditableImagePicker) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let editedImage = info[.editedImage] as? UIImage {
+                parent.selectedImage = editedImage
+            } else if let originalImage = info[.originalImage] as? UIImage {
+                parent.selectedImage = originalImage
+            }
+            
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+}
